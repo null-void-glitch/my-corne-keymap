d2a9ab3f5255cdda6aff1108084e03e0cbb41aa8
null-void-glitch <alexander.stumm@proton.me>
2025-05-24 23:23:59 +0200

    Update config.h

--- config.h
+++ config.h
@@ -37,8 +37,8 @@ timeout managed manually in keymap instead.
 
 
 // #define MASTER_LEFT
-#define MASTER_RIGHT
-// #define EE_HANDS
+// #define MASTER_RIGHT
+#define EE_HANDS
 
 //#define QUICK_TAP_TERM 0
 //#define TAPPING_TERM 100
